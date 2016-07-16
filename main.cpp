24b3e4b9cc57cc791a948f9d9e06f651a79f89f4
Gregory Gluszek <ggluszek@gmail.com>
2016-07-16 15:41:57 -0400

    Added Skeleton for Command Line Arg Parsing
    Planning on using long getopt methodology for obtaining input arguments. Basics
     are there so we can add parameters quickly as needed.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string>
+#include <getopt.h>
+
+/**
+ * Print application usage to stderr.
+ *
+ * \param[in] apExeName Executable name.
+ *
+ * \return None.
+ */
+void print_usage(const char* apExeName)
+{
+	//TODO
+}
 
 /**
  * Command line application entry point.
@@ -17,6 +31,39 @@
  */
 int main(int argc, char* argv[])
 {
+
+	std::string input_file = "";
+
+	// For command line arg parsing
+	int opt;
+	int option_index = 0;
+	static struct option long_options[] =
+	{
+		{"input-file", required_argument, 0, 'i'},
+		{"help", no_argument, 0, 'h'}
+	};
+
+	// Parse command line arguments
+	while ((opt = getopt_long(argc, argv, "i:h", long_options,
+		&option_index)) != -1)
+	{
+		switch (opt) {
+			case 'i':
+				input_file = optarg;
+				printf("Input Dir = %s\n", input_file.c_str());
+				break;
+
+			case 'h':
+				print_usage(argv[0]);
+				exit(EXIT_SUCCESS);
+				break;
+
+			default: /* '?' */
+				fprintf(stderr, "Unknown input argument.\n");
+				print_usage(argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
 
 	printf("Hello Wurld\n");
 
